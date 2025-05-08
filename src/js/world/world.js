bd1a8a87d4b6d5d49839ab5097344195152298be
hexianWeb <hexianWeb@gmail.com>
2025-05-08 14:44:00 +0800

    feat: 新增 相机跟随逻辑

--- src/js/world/world.js
+++ src/js/world/world.js
@@ -9,8 +9,8 @@ export default class World {
   constructor() {
     this.experience = new Experience()
     this.scene = this.experience.scene
+    this.camera = this.experience.camera
     this.resources = this.experience.resources
-
     this.scene.add(new THREE.AxesHelper(5))
 
     // Environment
@@ -19,6 +19,10 @@ export default class World {
       this.environment = new Environment()
       this.map = new Map()
       this.user = new User()
+      // 相机只跟随 user 的 agentGroup 水平移动
+      this.user.agentGroup.add(this.camera.instance)
+      this.user.agentGroup.add(this.environment.sunLight)
+
     })
   }
 
@@ -27,6 +31,8 @@ export default class World {
       this.map.update()
     }
     if (this.user) {
+      this.camera.instance.lookAt(this.user.agentGroup.position)
+      this.environment.sunLight.target.position.copy(this.user.agentGroup.position)
       this.user.update()
     }
   }
